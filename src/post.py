be8a25227a2a1afc96eacb5d5752b02883cac623
Sahil Thapar <sahilthapar>
2024-06-02 13:17:09 -0700

    Include bgg game parsing

--- src/post.py
+++ src/post.py
@@ -1,45 +1,68 @@
-from src.game import Game, get_game
-from src.user import User, get_user
+from typing import Optional
+from logging import getLogger
+from boardgamegeek import BGGClient, BGGItemNotFoundError
+from boardgamegeek.objects.games import BoardGame
+
+
+log = getLogger()
 
 
 class Post:
-    def __init__(self, game: str, user: str):
-        self.game = game
-        self.user = user
+    def __init__(self, contents: str, client: BGGClient):
+        self.contents = contents
         self.post_type = 'post'
         self.table_name = 'post'
+        self.bgg_client = client
+        self.game = self.get_game()
+        self.user = self.get_user()
+
+    def get_game(self) -> Optional[BoardGame]:
+        try:
+            log.info("Trying exact match")
+            game_exact = self.bgg_client.game(self.contents, exact=True)
+            if game_exact:
+                return game_exact
+        except BGGItemNotFoundError:
+            log.info("Failed to find exact match")
+            game_fuzzy = self.bgg_client.game(self.contents, exact=False)
+            return game_fuzzy
+
+
+    def get_user(self):
+        pass
 
 
 class SearchPost(Post):
-    def __init__(self, game: str, user: str):
-        super().__init__(game, user)
+    def __init__(self, contents: str, bgg_client: BGGClient):
+        super().__init__(contents, bgg_client)
         self.post_type = 'search'
         self.table_name = 'search'
 
 class InterestPost(Post):
-    def __init__(self, game: str, user: str):
-        super().__init__(game, user)
+    def __init__(self, contents: str, bgg_client: BGGClient):
+        super().__init__(contents, bgg_client)
         self.post_type = 'interest'
         self.table_name = 'interest'
 
 
 class SalePost(Post):
-    def __init__(self, game: str, user: str):
-        super().__init__(game, user)
+    def __init__(self, contents: str, bgg_client: BGGClient):
+        super().__init__(contents, bgg_client)
         self.post_type = 'sale'
         self.table_name = 'sale'
 
 
-def get_post(message: str) -> Post:
-    game = get_game(message)
-    user = get_user(message)
+def get_post(message: str, bgg_client: BGGClient) -> Post:
     message = message.lower()
 
-    if 'lookingfor' in message:
-        return SearchPost(game, user)
-    elif 'seekinginterest' in message:
-        return InterestPost(game, user)
-    elif 'sale' in message or 'selling' in message:
-        return SalePost(game, user)
-    return Post(game, user)
+    if '#lookingfor' in message:
+        contents = message.replace('#lookingfor', '').strip()
+        return SearchPost(contents, bgg_client)
+    elif '#seekinginterest' in message:
+        contents = message.replace('#seekinginterest', '').strip()
+        return InterestPost(contents, bgg_client)
+    elif '#sale' in message or '#selling' in message:
+        contents = message.replace('#sale', '').replace('#selling', '').strip()
+        return SalePost(contents, bgg_client)
+    return Post(message, bgg_client)
 
--- test/test_post.py
+++ test/test_post.py
@@ -1,26 +1,50 @@
 import pytest
+from contextlib import nullcontext
+from boardgamegeek import BGGClient
+from boardgamegeek.exceptions import BGGItemNotFoundError
+
 from src.post import Post, SearchPost, InterestPost, SalePost, get_post
-from src.game import Game
-from src.user import User
 
 class TestPost:
+
+    @pytest.fixture(name="bgg_client")
+    def bgg_client(self):
+        return BGGClient()
+
+    # @pytest.mark.parametrize(
+    #     argnames="message, expected_bgg_id",
+    #     argvalues=[
+    #         ("#lookingfor Monopoly", 1),
+    #         ("#selling Ark Nova", 2)
+    #     ],
+    #     ids=["with_link", "without_link"]
+    # )
+    # def test_get_game(self, bgg_client, message, expected_bgg_id):
+    #
+    #     post = Post(message, bgg_client)
+    #     assert expected_bgg_id == post.game.id
+
     @pytest.mark.parametrize(
-        argnames="message, expected_type, expected_table_name",
+        argnames="message, expected_type, expected_table_name, expected_game_id",
         argvalues=[
-            ("#lOokingFor", SearchPost, "search"),
-            ("#seekingInterest", InterestPost, "interest"),
-            ("#sale", SalePost, "sale"),
-            ("#selling something", SalePost, "sale"),
-            ("just a #message", Post, "post")
+            ("#lOokingFor monopoly", SearchPost, "search", nullcontext(1406)),
+            ("#seekingInterest Terraforming Mars", InterestPost, "interest", nullcontext(167791)),
+            ("#sale Guild of Merchant Explorers", SalePost, "sale", nullcontext(350933)),
+            ("#selling Lost Ruins of Arnak", SalePost, "sale", nullcontext(312484)),
+            ("just a #message no game", Post, "post", pytest.raises(BGGItemNotFoundError))
         ],
         ids=[
             "search", "interest", "sale", "sale-selling", "no-type"
         ]
 
     )
-    def test_get_post(self, message, expected_type, expected_table_name):
-        post = get_post(message)
+    def test_get_post(self, bgg_client, message, expected_type, expected_table_name, expected_game_id):
 
-        assert type(post) == expected_type
-        assert post.table_name == expected_table_name
+        with expected_game_id as e:
+            post = get_post(message, bgg_client)
+            assert type(post) == expected_type
+            assert post.bgg_client == bgg_client
+            assert post.table_name == expected_table_name
+            assert post.game.id == e
+
 
