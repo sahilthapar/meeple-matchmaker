a10e747d67aadcc82515c2b0c29138ce666350cb
Sahil Thapar <sahilthapar>
2024-06-09 10:23:48 -0700

    Remove obsolete files

--- src/thing.py
+++
@@ -1,3 +0,0 @@
-class Thing:
-    def __init__(self, name):
-        self.name = name
--- src/user.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self):
-        pass
-
-
-def get_user(message: str) -> str:
-    pass
