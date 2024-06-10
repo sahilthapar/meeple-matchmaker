0d23d67478378700a8b05c1d1fe7bc136501b7bd
Sahil Thapar <sahilthapar>
2024-06-10 15:26:10 -0700

    Fix database tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -11,19 +11,20 @@ class TestDatabase:
         message = mocker.patch("telegram.Message")
         message.text = "#seekingInterest Terraforming Mars"
         message.from_user.id = 101
+        message.from_user.first_name = "Jacob"
         return parse_message(message)
 
     @pytest.fixture(name="sample_data_tuple")
     def sample_data_tuple(self):
-        return 'sale', 167791, 'terraforming mars', '101', 1
+        return 'sale', 167791, '#seekinginterest terraforming mars', '101', 'Jacob', 1
 
     @pytest.fixture(name="sample_data_tuples")
     def sample_data_tuples(self):
         return [
-            ('sale', 167791, 'terraforming mars', '101', 1),
-            ('search', 167791, 'terraforming mars', '101', 1),
-            ('sale', 167791, 'terraforming mars', '101', 1),
-            ('search', 123, 'monopoly', '101', 1),
+            ('sale', 167791, '#seekinginterest terraforming mars', '101', 'Jacob', 1),
+            ('search', 167791, '#lookingfor terraforming mars', '102', 'Henry', 1),
+            ('sale', 167791, '#selling terraforming mars', '101', 'Jacob', 1),
+            ('search', 123, '#lookingfor monopoly', '101', 'Jacob', 1),
         ]
 
     @pytest.fixture(name="con")
@@ -34,7 +35,7 @@ class TestDatabase:
     def cursor(self, con):
         return con.cursor()
 
-    def test_post_db_init(self, cursor):
+    def test_init_post_db(self, cursor):
         init_post_db(cursor)
         data = cursor.execute("SELECT sql FROM sqlite_schema WHERE NAME = 'post'")
         assert len(list(data)) == 1
@@ -51,25 +52,25 @@ class TestDatabase:
     def test_read_post_db_by_id(self, con, cursor, post, sample_data_tuples):
         init_post_db(cursor)
         cursor.executemany(
-            'INSERT INTO post (post_type, game_id, contents, user, active) VALUES (?,?,?,?,?)',
+            'INSERT INTO post (post_type, game_id, text, user_id, user_name, active) VALUES (?,?,?,?,?,?)',
             sample_data_tuples
         )
         con.commit()
-
+        expected_data = [('102', 'Henry')]
         # by game id
-        data = read_post_db(cursor, game_id=[167791, 321])
-        assert list(data) == list(filter(lambda x: x[1] in [167791], sample_data_tuples))
+        data = read_post_db(cursor, game_id=167791, post_type="search")
+        assert list(data) == expected_data
 
     def test_read_post_db_by_type(self, con, cursor, post, sample_data_tuples):
         init_post_db(cursor)
         cursor.executemany(
-            'INSERT INTO post (post_type, game_id, contents, user, active) VALUES (?,?,?,?,?)',
+            'INSERT INTO post (post_type, game_id, text, user_id, user_name, active) VALUES (?,?,?,?,?,?)',
             sample_data_tuples
         )
         con.commit()
-
+        expected_data = [('101', 'Jacob')]
         # by game id
-        data = read_post_db(cursor, post_type="search")
-        assert list(data) == list(filter(lambda x: x[0] == "search", sample_data_tuples))
+        data = read_post_db(cursor, game_id=167791, post_type="sale")
+        assert list(data) == expected_data
 
 
