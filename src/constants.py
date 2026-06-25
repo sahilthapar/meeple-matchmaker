f2c26a299f52f7deca565a7e150c54dba7d13a7e
Prithvi Kumar <prithvikumar@hotmail.com>
2026-06-25 14:43:35 +0530

    Added error handler to post exception data to a telegram group

--- src/constants.py
+++ src/constants.py
@@ -6,6 +6,10 @@ MEEPLE_MARKET_CHAT_ID = -1001243715567
 # Meeple dev chat id for local dev
 # MEEPLE_MARKET_CHAT_ID = -1003918412989
 
+ERROR_GROUP_CHAT_ID = -5334001786
+# Personal Error group chat id for local dev
+# ERROR_GROUP_CHAT_ID = -1003918412989
+
 ADMIN_IDS = [
     995823071,   # Sahil Thapar
     6946013582,  # Mica
